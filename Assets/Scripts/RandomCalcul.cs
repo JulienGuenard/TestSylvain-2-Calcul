cb266e7e5e3e1614baaaa837b8ca776053cd1d7a
Archer01 <julienguenard01@gmail.com>
2022-09-20 16:49:44 +0200

    commit projet unity
    commit projet unity

---
+++ Assets/Scripts/RandomCalcul.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RandomCalcul : MonoBehaviour
+{
+
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
